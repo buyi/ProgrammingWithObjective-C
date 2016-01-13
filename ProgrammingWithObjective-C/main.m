78c7d27788bae52d483c742c51514e655de4855e
buyi <huaqirong@126.com>
2016-01-13 10:18:13 +0800

    init project

---
+++ ProgrammingWithObjective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ProgrammingWithObjective-C
+//
+//  Created by buyi on 16/1/13.
+//  Copyright © 2016年 buyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
